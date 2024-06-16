7e54a85bae11506516157c2d53c1de996a8245ba
HansCode20 <fananiilham20@gmail.com>
2024-06-16 11:20:32 +0700

    Perubahan

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['./node_modules/sonner'],
+      external: ['sonner'],
 
       output: {
         globals: {
-          sonner: './node_modules/Sonner'
+          sonner: 'Sonner'
         }
       }
     }
